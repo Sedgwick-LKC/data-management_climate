44daf1149c391fc1c50e3b4b2dd051d262b986db
njlyon0 <nicholasjlyon@gmail.com>
2025-11-07 14:03:43 -0500

    dev (precip): partial instructions for downloading data

--- precipitation/precip-01_download.r
+++ precipitation/precip-01_download.r
@@ -17,6 +17,15 @@ rm(list = ls()); gc()
 # MesoWest Instructions ----
 ## --------------------------------- ##
 
-# Link: https://mesowest.utah.edu/cgi-bin/droman/mesomap.cgi?state=CA&rawsflag=3
+# In order to download precipitation data for the LKC, do the following:
+## 1. Go here: https://mesowest.utah.edu/cgi-bin/droman/mesomap.cgi?state=CA&rawsflag=3
+## 2. Double check that "Region/Zone" (in top left corner) is set to "CALIFORNIA"
+## 3. Set "Network" to "RAWS"
+## 4. Click "Refresh Map" button
+## 5. Zoom in on Santa Barbara
+## 6. Click on station "SXWC1"
+### Station is east of Lompoc, north of Santa Barbara (in Santa Ynez region)
+## 7. Select "Precip" button in "View" options
+## 8. Click "Download Data" (beneath the scatterplot)
 
 # End ----
