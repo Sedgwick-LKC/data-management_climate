06a3b47295f2ea216a2dd3f7557f362048776579
njlyon0 <nicholasjlyon@gmail.com>
2025-11-10 09:12:15 -0500

    docs (precip): tweaking instructions to be more general

--- precipitation/precip-01_download.r
+++ precipitation/precip-01_download.r
@@ -19,12 +19,12 @@ rm(list = ls()); gc()
 
 # In order to download precipitation data for the LKC, do the following:
 ## 1. Go to Synoptic Data (https://synopticdata.com/)
-## 2. Click "Login" (top right corner)
-## 3. Click "Sign up" and create _FREE_ account (email, name, some other info)
-## 4. Under "My Profile", click "Download data"
-## 5. Under "1. Choose station", enter "SXWC1"
-## 6. Under "2. Dates", enter desired date range
-## 7. Under "3. Variables and units", check the box to the left of "Precipitation accumulated"
+## 2. Click "Login" or "Sign up" (top right corner)
+### When you make an account, make sure it's _FREE_
+## 3. Under "My Profile", click "Download data"
+## 4. Under "1. Choose station", enter "SXWC1"
+## 5. Under "2. Dates", enter desired date range
+## 6. Under "3. Variables and units", check the box to the left of "Precipitation accumulated"
 
 ## --------------------------------- ##
 # Data Use Instructions ----
