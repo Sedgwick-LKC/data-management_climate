2344c5ed3da6f99e73b37906e3e247f0e3d824d4
njlyon0 <nicholasjlyon@gmail.com>
2025-11-10 10:49:17 -0500

    refactor (precip): implementing `parse_synoptic` to streamline initial loading/wrangling of data

--- precipitation/precip-02_wrangle.r
+++ precipitation/precip-02_wrangle.r
@@ -13,6 +13,10 @@ source(file = file.path("00_setup.r"))
 # Clear environment
 rm(list = ls()); gc()
 
+# Load custom functions
+purrr::walk(.x = dir(path = file.path("tools"), pattern = "fxn_"),
+            .f = ~ source(file = file.path("tools", .x)))
+
 ## --------------------------------- ##
 # Load Data ----
 ## --------------------------------- ##
@@ -24,50 +28,29 @@ rm(list = ls()); gc()
 # Identify just the last file (if more than one exists, it's probably earlier/later downloads)
 (raw_prec <- file.path("data", "precip", local_prec[length(local_prec)]))
 
-# The downloaded table as-is is malformed so we need to be tricky about reading it in
-prec_v01 <- data.frame("x" = readLines(con = raw_prec))
-
-# Check the first few rows of the data
-head(prec_v01, n = 10)
-
-# Need to ditch some dumb/bad header rows
-prec_v02 <- prec_v01 |> 
-  dplyr::filter(stringr::str_detect(string = x, pattern = "#") != T) |> 
-  dplyr::filter(x %in% c("Station_ID,Date_Time,precip_accum_set_1", ",,Millimeters") != T)
-
-# Re-check some rows
-head(prec_v02)
-
-## --------------------------------- ##
-# Generic Column Creation ----
-## --------------------------------- ##
-
-# Let's get all the columns that we need
-prec_v03 <- prec_v02 |> 
-  # Separate out the columns we want
-  tidyr::separate_wider_delim(cols = x, delim = ",",
-                              names = c("station", "date_time", "precip.accum_mm")) |> 
-  # Fix column class issues
-  dplyr::mutate(precip.accum_mm = as.numeric(precip.accum_mm)) |> 
-  # Add some columns for info we stripped out of the bad header rows
-  dplyr::mutate(elev_ft = 1402.0,
-                lat_deg = 34.680630,
-                lon_deg = -120.047440,
-                .after = station) |> 
-  # Convert elevation to meters
-  dplyr::mutate(elevation_m = (elev_ft / 3.281),
-                .after = elev_ft) |> 
-  dplyr::select(-elev_ft)
+# The downloaded data are malformed coming from Synoptic Data (bad header rows)
+## so we need to use a custom function to load it and get it into real rows/columns
+prec_v01 <- parse_synoptic(csv = raw_prec)
 
 # Check structure
-dplyr::glimpse(prec_v03)
+dplyr::glimpse(prec_v01)
+
+# Let's go ahead and rename these slightly
+prec_v02 <- prec_v01 |> 
+  # Make them lowercase
+  dplyr::rename_with(.fn = tolower) |> 
+  # Simplify precip column name
+  dplyr::rename(precip.accum_mm = precip_accum_set_1_millimeters)
+
+# Check structure
+dplyr::glimpse(prec_v02)
 
 ## --------------------------------- ##
 # Date/Time Wrangling ----
 ## --------------------------------- ##
 
 # Need to get date/time into a use-able format
-prec_v04 <- prec_v03 |> 
+prec_v03 <- prec_v02 |> 
   # Strip out date 
   dplyr::mutate(date = as.Date(stringr::str_sub(string = date_time,
                                                 start = 1, end = 10)),
@@ -78,14 +61,14 @@ prec_v04 <- prec_v03 |>
   dplyr::distinct()
   
 # Check structure
-dplyr::glimpse(prec_v04)
+dplyr::glimpse(prec_v03)
 
 ## --------------------------------- ##
 # Export ----
 ## --------------------------------- ##
 
 # Make a final object
-prec_v99 <- prec_v04
+prec_v99 <- prec_v03
 
 # Re-check structure
 dplyr::glimpse(prec_v99)
